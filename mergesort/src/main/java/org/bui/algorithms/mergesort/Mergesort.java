a6fdd692fb2080a07cf37e8fee8a03f940b557ce
Thai Bui <thai.bui@bazaarvoice.com>
2016-03-17 15:52:45 -0700

    Finish mergesort implementation, TODO in-place merge phase

--- mergesort/src/main/java/org/bui/algorithms/mergesort/Mergesort.java
+++ mergesort/src/main/java/org/bui/algorithms/mergesort/Mergesort.java
@@ -1,11 +1,109 @@
 package org.bui.algorithms.mergesort;
 
+import java.lang.reflect.Array;
+
+/**
+ * A simple implementation of the mergesort algorithm. This implementation tries its best to do an in-place
+ * sort and merge, however, the in-place merge is still a work in progress and incomplete.
+ *
+ * TODO Implement in-place merge phase
+ */
 public class Mergesort {
-  public static <T> T[] sort(T[] elements){
+
+  /**
+   * Perform an in-place merge and sort on a given array of {@link Comparable} element. This method tries
+   * not to increase the memory footprint by performing an in-place sort & merge.
+   *
+   * @param elements The array of {@link Comparable} element
+   * @param classOf The class of the element
+   * @param <T> The generic type of the element
+   * @return The original array with its element sorted using the mergesort algorithm
+   */
+  public static <T extends Comparable> T[] sort(T[] elements, Class<T> classOf) {
     if (null == elements){
       throw new NullPointerException("array of elements cannot be null");
     }
 
+    if (elements.length > 1){
+      int mid = (int)Math.floor(elements.length / 2);
+      sort(elements, 0, mid, mid, elements.length - mid, classOf);
+    }
+
     return elements;
+  }
+
+  private static <T extends Comparable> void sort(T[] a, int leftIdx, int leftLength, int rightIdx, int rightLength, Class<T> classOf) {
+    if (leftLength > 2) {
+      int mid = (int)Math.floor(leftLength / 2);
+      sort(a, leftIdx, mid, leftIdx + mid, leftLength - mid, classOf);
+    } else if (leftLength == 2){
+      compareSwap(a, leftIdx, leftIdx + 1);
+    }
+
+    if (rightLength > 2){
+      int mid = (int)Math.floor(rightLength / 2);
+      sort(a, rightIdx, mid, rightIdx + mid, rightLength - mid, classOf);
+    } else if (rightLength == 2){
+      compareSwap(a, rightIdx, rightIdx + 1);
+    }
+
+    merge(a, leftIdx, leftLength, rightIdx, rightLength, classOf);
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <T extends Comparable> void merge(T[] a, int leftIdx, int leftLength, int rightIdx, int rightLength, Class<T> classOf){
+    T[] tmp = (T[]) Array.newInstance(classOf, leftLength + rightLength);
+    int left = leftIdx;
+    int right = rightIdx;
+    int idx = 0;
+
+    while (idx < leftLength + rightLength){
+      if (left == leftIdx + leftLength) {
+        tmp[idx] = a[right];
+        right++;
+      }
+
+      else if (right == rightIdx + rightLength) {
+        tmp[idx] = a[left];
+        left++;
+      }
+
+      else if (compare(a, left, right) < 0) {
+        tmp[idx] = a[left];
+        left++;
+      }
+
+      else {
+        tmp[idx] = a[right];
+        right++;
+      }
+
+      idx++;
+    }
+
+    System.arraycopy(tmp, 0, a, leftIdx, tmp.length);
+  }
+
+  private static <T extends Comparable> void compareSwap(T[] a, int l, int r){
+    if (compare(a, l, r) > 0){
+      swap(a, l, r);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <T extends Comparable> int compare(T[] a, int l, int r){
+    if (null == a[l] || null == a[r]) {
+      throw new NullPointerException(
+          String.format("array element cannot be null, found null element at index %d", a[l] == null ? l : r)
+      );
+    }
+
+    return a[l].compareTo(a[r]);
+  }
+
+  private static <T> void swap(T[] a, int l, int r){
+    T tmp = a[l];
+    a[l] = a[r];
+    a[r] = tmp;
   }
 }
--- mergesort/src/test/java/org/bui/algorithms/mergesort/MergesortTest.java
+++ mergesort/src/test/java/org/bui/algorithms/mergesort/MergesortTest.java
@@ -1,14 +1,108 @@
 package org.bui.algorithms.mergesort;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertArrayEquals;
 
 public class MergesortTest {
 
+  @Rule
+  public final ExpectedException expectedException = ExpectedException.none();
+
   @Test
   public void testSortEmptyIterableWorks() {
-    assertArrayEquals("Sort empty array should work", new String[]{}, Mergesort.sort(new String[]{}));
+    assertArrayEquals("Sort empty array should work", new String[]{}, Mergesort.sort(new String[]{}, String.class));
   }
 
+  @Test
+  public void testNullArrayOfElementsShouldFail() {
+    expectedException.expect(NullPointerException.class);
+    expectedException.expectMessage("array of elements cannot be null");
+    Mergesort.sort(null, Comparable.class);
+  }
+
+  @Test
+  public void testArrayWithNullElementShouldFail() {
+    expectedException.expect(NullPointerException.class);
+    expectedException.expectMessage(String.format("array element cannot be null, found null element at index %d", 2));
+    Mergesort.sort(new Integer[]{1,2,null,3,5}, Integer.class);
+  }
+
+
+  @Test
+  public void testSortArrayWithTwoElements() {
+    assertArrayEquals("Array with two elements should be sorted correctly",
+        new Integer[]{1, 2}, Mergesort.sort(new Integer[]{2, 1}, Integer.class));
+  }
+
+  @Test
+  public void testSortArrayWithOneElement() {
+    assertArrayEquals("Array with one element should be sorted correctly",
+        new Integer[]{1}, Mergesort.sort(new Integer[]{1}, Integer.class));
+  }
+
+  @Test
+  public void testSortArrayWithManyElementsShouldBeCorrect() {
+    assertArrayEquals("Array in reversed order should be sorted correctly",
+        new Integer[]{15,62,105,105},
+        Mergesort.sort(new Integer[]{105,105,62,15}, Integer.class)
+    );
+
+    assertArrayEquals("Array in random order should be sorted correctly",
+        new Integer[]{1,1,1,2,3,4,4,5,6,8,60,123},
+        Mergesort.sort(new Integer[]{8,1,4,2,4,1,5,60,1,3,6,123}, Integer.class)
+    );
+  }
+
+  @Test
+  public void testSortDifferentDataTypeWorks() {
+    assertArrayEquals("Sort array of String should work",
+        new String[]{"a", "b", "c"},
+        Mergesort.sort(new String[]{"b", "c", "a"}, String.class));
+
+    assertArrayEquals("Sort array of object with custom comparable class should work",
+        new House[]{House.of("Jerry"), House.of("Ben"), House.of("Adam")},
+        Mergesort.sort(new House[]{House.of("Jerry"), House.of("Adam"), House.of("Ben")}, House.class)
+    );
+  }
+
+  private static class House implements Comparable {
+    private final String name;
+
+    public House(String name){
+      if (null == name){
+        throw new NullPointerException("House's name cannot be null");
+      }
+
+      this.name = name;
+    }
+
+    public static House of(String name){
+      return new House(name);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+      return ((House) o).name.compareTo(name); // descending order
+    }
+
+    @Override
+    public String toString(){
+      return name;
+    }
+
+    @Override
+    public boolean equals(Object o){
+      return o != null
+          && getClass().equals(o.getClass())
+          && name.equals(((House)o).name);
+    }
+
+    @Override
+    public int hashCode(){
+      return name.hashCode();
+    }
+  }
 }
